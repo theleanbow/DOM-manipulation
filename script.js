a25071b791a11dfca7cf3884a26e034396057883
LeanBow <theleanbow@gmail.com>
2022-05-09 10:07:29 +0545

    Add an <h3> with blue text that says ''I’m a blue h3!''

--- script.js
+++ script.js
@@ -20,3 +20,9 @@ const p = document.createElement("p");
 p.textContent = "Hey I'm red!";
 p.style.color = "red";
 container.appendChild(p);
+
+//2) an <h3> with blue text that says “I’m a blue h3!”
+const h3 = document.createElement("h3");
+h3.textContent = "I'm a blue h3";
+h3.style.color = "blue";
+container.appendChild(h3);
